d231ff5bd1eb4639aac3ff4e9921bbef6b6d68d2
helios <helios@polispay.org>
2018-12-11 13:06:45 -0600

    Debugg

--- TicTacToe.py
+++ TicTacToe.py
@@ -5,7 +5,7 @@ from imutils.video import VideoStream
 
 webcam = cv.VideoCapture(0)
 # webcam = VideoStream(0)
-app = TicTacToeApp(webcam, 640, 360, debug=False)
+app = TicTacToeApp(webcam, 640, 360, debug=True)
 app.set_red_calibration(200, 250)
 app.root.mainloop()
 
